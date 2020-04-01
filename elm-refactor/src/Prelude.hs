e101344f8a35a52e9e2d152a8273bf6b34189374
Aaron VonderHaar <gruen0aermel@gmail.com>
2020-03-31 21:49:32 -0700

    elm-refactor: add basic CLI test

--- elm-refactor/src/Prelude.hs
+++ elm-refactor/src/Prelude.hs
@@ -1,6 +1,8 @@
 -- TODO: move to elm-format, and combine with other Prelude.hs in this repo
 module Prelude
     ( module Relude
+    , module Elm.Utils
     ) where
 
-import Relude hiding (exitSuccess, exitFailure)
+import Relude hiding ((>>), exitSuccess, exitFailure)
+import Elm.Utils hiding (run)
--- elm-refactor/test/ElmRefactor/CliTest.hs
+++ elm-refactor/test/ElmRefactor/CliTest.hs
@@ -1,10 +1,11 @@
 module ElmRefactor.CliTest (tests) where
 
-import Elm.Utils ((|>))
 import Test.Tasty
+import Test.Tasty.HUnit
 
-import CommandLine.TestWorld (TestWorld, run, goldenExitStdout)
+import CommandLine.TestWorld hiding (uploadFile)
 import qualified CommandLine.TestWorld as TestWorld
+import qualified CommandLine.World as World
 import qualified ElmRefactor.Cli as ElmRefactor
 
 
@@ -13,7 +14,38 @@ tests =
     testGroup "CLI"
         [ goldenExitStdout "usage instructions" 0 "test/usage.stdout" $ world
             |> run "elm-refactor" [ "--help" ]
+        , testCase "--upgrade" $ world
+            |> uploadFile "Main.elm"
+                [ "module Main exposing (..)"
+                , "import Data.Maybe"
+                , "x = Data.Maybe.maybe 0 increment"
+                ]
+            |> uploadFile "upgrade.elm"
+                [ "import Maybe"
+                , "upgrade_Data_Maybe_maybe onNothing onJust maybe = maybe |> Maybe.map onJust |> Maybe.withDefault onNothing"
+                ]
+            |> run "elm-refactor" [ "--upgrade", "upgrade.elm", "Main.elm" ]
+            |> assertFile "Main.elm"
+                [ "module Main exposing (..)"
+                , ""
+                , "import Maybe"
+                , ""
+                , ""
+                , "x ="
+                , "    \\maybe -> maybe |> Maybe.map increment |> Maybe.withDefault 0"
+                ]
         ]
+
+
+uploadFile :: FilePath -> [Text] -> TestWorld -> TestWorld
+uploadFile filename content =
+    TestWorld.uploadFile filename (unlines content)
+
+
+assertFile :: FilePath -> [Text] -> TestWorld -> Assertion
+assertFile filename expectedContent =
+    TestWorld.eval (World.readUtf8File filename)
+        >> assertEqual ("assertFile " ++ filename) (unlines expectedContent)
 
 
 world :: TestWorld
