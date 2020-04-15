7a49d1291fec0262331b081332303f8478c46b73
Aaron VonderHaar <gruen0aermel@gmail.com>
2020-04-15 11:14:43 -0700

    Extract refactorExpressionTest

--- elm-refactor/test/ElmRefactor/Integration/SimplifyTest.hs
+++ elm-refactor/test/ElmRefactor/Integration/SimplifyTest.hs
@@ -15,49 +15,54 @@ import qualified ElmFormat.Upgrade_0_19
 import qualified ElmVersion
 
 
+test_tests :: TestTree
+test_tests =
+    testGroup "simplifying code"
+        [ refactorExpressionTest "evaluates `==` comparisons"
+            [ "A.f \"special\"" ]
+            [ "upgrade_A_f s = s == \"special\"" ]
+            [ "True" ]
+        , refactorExpressionTest "does not simplify binops from the source file"
+            [ "\"special\" == \"other\"" ]
+            [ "upgrade_X_x = ()" ]
+            [ "\"special\" == \"other\"" ]
+        , refactorExpressionTest "evaluates `++`"
+            [ "A.f [1, 2] [3, 4]" ]
+            [ "upgrade_A_f a b = a ++ b" ]
+            [ "[ 1, 2, 3, 4 ]" ]
+        ]
+
+
+{-| This just makes tests more concise by building the input and expected output files from the given expressions. -}
+refactorExpressionTest :: String -> [Text] -> [Text] -> [Text] -> TestTree
+refactorExpressionTest testName inputExpression upgradeFile expectedOutputExpression =
+    refactorTest testName
+        (fileWithExpression inputExpression)
+        upgradeFile
+        (fileWithExpression expectedOutputExpression)
+    where
+        fileWithExpression e =
+          [ "module Main exposing (..)"
+          , ""
+          , "import A"
+          , ""
+          , ""
+          , "keepAImport ="
+          , "    A.keep"
+          , ""
+          , ""
+          , "x ="
+          ]
+          ++
+          fmap ((<>) "    ") e
+
+
 refactorTest :: String -> [Text] -> [Text] -> [Text] -> TestTree
 refactorTest testName inputFile upgradeFile expectedOutputFile =
     testCase testName $
         assertEqual "output should be as expected"
             (Right $ unlines expectedOutputFile)
             (refactor (unlines upgradeFile) (unlines inputFile))
-
-test_tests :: TestTree
-test_tests =
-    testGroup "simplifying code"
-        [ refactorTest "evaluates `==` comparisons"
-            [ "import A exposing (a)"
-            , "x = A.f \"special\""
-            ]
-            [ "upgrade_A_f s = s == \"special\"" ]
-            [ "module Main exposing (x)"
-            , ""
-            , ""
-            , "x ="
-            , "    True"
-            ]
-        , refactorTest "does not simplify binops from the source file"
-            [ "x = \"special\" == \"other\""
-            ]
-            [ "upgrade_X_x = ()" ]
-            [ "module Main exposing (x)"
-            , ""
-            , ""
-            , "x ="
-            , "    \"special\" == \"other\""
-            ]
-        , refactorTest "evaluates `++`"
-            [ "import A exposing (a)"
-            , "x = A.f [1, 2] [3, 4]"
-            ]
-            [ "upgrade_A_f a b = a ++ b" ]
-            [ "module Main exposing (x)"
-            , ""
-            , ""
-            , "x ="
-            , "    [ 1, 2, 3, 4 ]"
-            ]
-        ]
 
 
 refactor :: Text -> Text -> Either [String] Text.Text
