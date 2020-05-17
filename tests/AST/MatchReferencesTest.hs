46f292756395ff371c6112c23a3ce8edf5fd2275
Aaron VonderHaar <gruen0aermel@gmail.com>
2020-05-16 18:07:51 -0700

    Simplify setup in AST.MatchReferencesTest

--- tests/AST/MatchReferencesTest.hs
+++ tests/AST/MatchReferencesTest.hs
@@ -5,13 +5,16 @@ import Elm.Utils ((|>))
 
 import AST.V0_16
 import AST.MatchReferences
-import AST.Module (ImportMethod(..), DetailedListing(..))
-import AST.Listing (Listing(..))
+import AST.Module (ImportMethod(..))
 import Data.Functor.Identity
 import qualified Data.Indexed as I
 import Expect
 import ElmFormat.ImportInfo (ImportInfo)
+import ElmVersion
 import qualified ElmFormat.ImportInfo as ImportInfo
+import qualified Parse.Module
+import qualified Parse.Parse as Parse
+import qualified Reporting.Result as Result
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -24,50 +27,50 @@ tests =
     testGroup "AST.MatchReferences"
     [ testGroup "matchReferences" $
         let
-            makeImportInfo :: [(String, List String, Maybe String, Listing DetailedListing)] -> ImportInfo [UppercaseIdentifier]
-            makeImportInfo imports =
+            makeImportInfo :: [(String, List String)] -> [String] -> ImportInfo [UppercaseIdentifier]
+            makeImportInfo knownContentsRaw imports =
                 let
-                    knownContents = imports |> fmap makeKnownContent |> Dict.fromList
+                    knownContents = knownContentsRaw |> fmap makeKnownContent |> Dict.fromList
                 in
                 ImportInfo.fromImports (fromMaybe mempty . flip Dict.lookup knownContents)
                 (imports |> fmap makeImportMethod |> Dict.fromList)
 
-            makeKnownContent (moduleName, known, _, _) =
+            makeKnownContent (moduleName, known) =
                 ( fmap UppercaseIdentifier $ splitOn "." moduleName
                 , fmap (VarName . LowercaseIdentifier) known
                 )
 
-            makeImportMethod :: (String, knownContents, Maybe String, Listing DetailedListing) -> ([UppercaseIdentifier], ImportMethod)
-            makeImportMethod (moduleName, _, as, exposing) =
-                ( fmap UppercaseIdentifier $ splitOn "." moduleName
-                , ImportMethod
-                    (fmap (C ([], []) . UppercaseIdentifier) as)
-                    (C ([], []) exposing)
-                )
-
-            closedListing = ClosedListing
-            openListing = OpenListing (C ([], []) ())
+            makeImportMethod :: String -> ([UppercaseIdentifier], ImportMethod)
+            makeImportMethod importString =
+                case Result.toMaybe $ Parse.parse importString (Parse.Module.import' Elm_0_19) of
+                    Nothing -> undefined -- Not handled: fix the test input to parse correctly
+                    Just (C _ moduleName, importMethod) ->
+                        (moduleName, importMethod)
 
             test ::
                 String
-                -> [(String, List String, Maybe String, Listing DetailedListing)]
+                -> [(String, List String)] -- knownContents
+                -> [String] -- imports
                 -> Ref [UppercaseIdentifier]
                 -> Ref (MatchedNamespace [UppercaseIdentifier])
                 -> TestTree
-            test name imports sourceAst matchedAst =
+            test name knownContents imports sourceAst matchedAst =
                 testCase name $
-                    matchReferences (makeImportInfo imports) (I.Fix $ Identity $ VarExpr sourceAst)
+                    matchReferences (makeImportInfo knownContents imports) (I.Fix $ Identity $ VarExpr sourceAst)
                         |> Expect.equals (I.Fix $ Identity $ VarExpr matchedAst)
         in
-        [ test "identifies unknown references" []
+        [ test "identifies unknown references"
+            [] []
             (VarRef [UppercaseIdentifier "A"] (LowercaseIdentifier "a"))
             (VarRef (Unmatched [UppercaseIdentifier "A"]) (LowercaseIdentifier "a"))
         , test "matches references from an import"
-            [ ("A", [], Nothing, closedListing) ]
+            []
+            [ "import A" ]
             (VarRef [UppercaseIdentifier "A"] (LowercaseIdentifier "a"))
             (VarRef (MatchedImport True [UppercaseIdentifier "A"]) (LowercaseIdentifier "a"))
         , test "matches reference to a known value via exposing(..)"
-            [ ("Html", [ "div" ], Nothing, openListing) ]
+            [ ("Html", ["div"]) ]
+            [ "import Html exposing (..)" ]
             (VarRef [] (LowercaseIdentifier "div"))
             (VarRef (MatchedImport False [UppercaseIdentifier "Html"]) (LowercaseIdentifier "div"))
         ]
